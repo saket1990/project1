20cd71b0a3d2c8b3a40c01e06351622bac5b504f
arjunvip100 <Arjunvip100@gmail.com>
2022-06-22 16:46:22 +0530

    day2ProjecPhase1Completed

--- src/controllers/BlogsController.js
+++ src/controllers/BlogsController.js
@@ -18,4 +18,46 @@ const createBlog = async function (req, res) {
         });
     }
 };
+
+const findBlogs = async function (req, res) {
+
+    try {
+        const allBlogs = await blogModel.find({ $and: [{ isDeleted: false}, { isPublished: false}] });
+        if (allBlogs == false) return res.status(404).send({ status: false, msg: "No such Blogs are present" });
+        res.status(200).send({ status: true, msg: "Found Blogs", data: allBlogs })
+        
+    } catch (err) {
+        res.status(500).send({
+            status: false,
+            msg: err.message,
+        })
+    }
+
+}
+
+const filterBlogs = async function (req, res) {
+
+    try {
+
+        const auth = req.query.authorId
+        const cat = req.query.category
+        const tag = req.query.tags
+        const subcat = req.query.subcategory
+        // const allBlogs = await BlogsModel.find({ $and: [{ isDeleted: false }, { isPublished: false }] });
+        const gotBlogs = await blogModel.find({ $or: [{ authorId: auth }, { category: cat }, { tags: tag }, { subcategory: subcat }] });
+        if (gotBlogs == false) return res.status(404).send({ status: false, msg: "no such blogs are present" })
+        res.status(201).send({ status: true, msg: "Found Blogs okey", data: gotBlogs })
+    } catch (err) {
+        res.status(500).send({
+            status: false,
+            msg: err.message,
+        })
+    }
+
+}
+
+
+
+module.exports.findBlogs = findBlogs
+module.exports.filterBlogs = filterBlogs
 module.exports.createBlog= createBlog
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const AuthorController= require("../controllers/authorController");
 const BlogsController= require("../controllers/BlogsController");
-
+const updateController= require("../controllers/updateController");
+const deleteController= require("../controllers/deleteController");
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -11,5 +12,9 @@ router.get("/test-me", function (req, res) {
 
 router.post("/authors", AuthorController.createAuthor)
 router.post("/blogs",BlogsController.createBlog)
-
+router.get("/getblogs",BlogsController.findBlogs)
+router.get("/filterblogs",BlogsController.filterBlogs)
+router.put("/updateblogs/:blogid",updateController.updateBlogs)
+router.delete("/delblogs/:blogid",deleteController.deleteBlog)
+router.delete("/delblogbyparam",deleteController.deleteBlogsByparams )
 module.exports = router;
