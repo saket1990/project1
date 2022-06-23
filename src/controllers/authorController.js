43f253d1de32fc8a11926cbd091f5ae820199c25
arjunvip100 <Arjunvip100@gmail.com>
2022-06-23 18:28:07 +0530

    Phase2

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,4 +1,5 @@
 const authorModel = require("../models/authorModel");
+const jwt = require("jsonwebtoken");
 
 
 const createAuthor= async function(req, res) {
@@ -7,5 +8,21 @@ const createAuthor= async function(req, res) {
    res.send({status: true, msg: savedData})
     }
 
+    const loginUser = async function (req, res) {
+        try {
+            let userName = req.body.email
+            let password = req.body.password
+            let user = await authorModel.findOne({ email: userName, password: password })
+            if (!user)
+                return res.status(404).send({status: false,msg: "username or the password is not correct"})
+            let token = jwt.sign({ userId: user._id.toString(),
+                batch: "thorium",
+                organisation: "FUnctionUp",},"functionup-thorium")   
+            res.status(200).send({ status: true, data: token, authorId: user._id  })   
+        } catch (err) {
+            res.status(500).send({ msg: "Error", error: err.message })
+        }
+    }
+module.exports.loginUser= loginUser
 module.exports.createAuthor= createAuthor
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -4,6 +4,7 @@ const AuthorController= require("../controllers/authorController");
 const BlogsController= require("../controllers/BlogsController");
 const updateController= require("../controllers/updateController");
 const deleteController= require("../controllers/deleteController");
+const Middleware = require("../middlewares/auth");
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -11,10 +12,13 @@ router.get("/test-me", function (req, res) {
 
 
 router.post("/authors", AuthorController.createAuthor)
-router.post("/blogs",BlogsController.createBlog)
+router.post("/blogs/:userId",Middleware.authenticate,Middleware.authorise, BlogsController.createBlog)
 router.get("/getblogs",BlogsController.findBlogs)
 router.get("/filterblogs",BlogsController.filterBlogs)
-router.put("/updateblogs/:blogid",updateController.updateBlogs)
+router.put("/updateblogs/:blogid",Middleware.authenticate,Middleware.authorise,updateController.updateBlogs)
 router.delete("/delblogs/:blogid",deleteController.deleteBlog)
 router.delete("/delblogbyparam",deleteController.deleteBlogsByparams )
+router.post("/loginUser",AuthorController.loginUser)
+
+
 module.exports = router;
